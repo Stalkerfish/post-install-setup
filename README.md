bbdfb6c90794905c8718bb723d217cf98d7e8f60
Stalkerfish <silva.allison2002@gmail.com>
2021-07-15 02:10:55 -0300

    update for personal apps

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # post-install-setup
-This repo stores automation scripts for post-install setups for linux-based systems. (Fork as you wish, but the script is personal)
+This repo stores automation scripts for post-install setups for linux-based systems. (Fork as you wish, but the script configures the system for my personal workflow, so adapt to yours)
--- post-install.sh
+++ post-install.sh
@@ -1,31 +1,45 @@
 #!/usr/bin/env bash
 # ----------------------------- VARIÁVEIS ----------------------------- #
-PPA_LIBRATBAG="ppa:libratbag-piper/piper-libratbag-git"
 PPA_LUTRIS="ppa:lutris-team/lutris"
-PPA_GRAPHICS_DRIVERS="ppa:graphics-drivers/ppa"
+PPA_ULAUNCHER="ppa:agornostal/ulauncher"
+PPA_APPIMAGELAUNCHER="ppa:appimagelauncher-team/stable"
 
+
+URL_PPA_VIPER4LINUX="https://thepbone.github.io/PPA-Repository/thepbone_ppa.list"
 URL_WINE_KEY="https://dl.winehq.org/wine-builds/winehq.key"
 URL_PPA_WINE="https://dl.winehq.org/wine-builds/ubuntu/"
-URL_GOOGLE_CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-URL_SIMPLE_NOTE="https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb"
 URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.9.2-1_amd64.deb"
-URL_INSYNC="https://d2t3ff60b2tol4.cloudfront.net/builds/insync_3.0.20.40428-bionic_amd64.deb"
+URL_GITHUB_DESKTOP="https://github.com/shiftkey/desktop/releases/download/release-2.9.0-linux3/GitHubDesktop-linux-2.9.0-linux3.deb"
+URL_WEB_TORRENT_DESKTOP="https://github.com/webtorrent/webtorrent-desktop/releases/download/v0.24.0/webtorrent-desktop_0.24.0_amd64.deb"
+URL_VIPER4LINUX_KEY="https://thepbone.github.io/PPA-Repository/KEY.gpg"
 
-DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
+
+DIRETORIO_DOWNLOADS="$HOME/Downloads/pacotes\ deb"
 
 PROGRAMAS_PARA_INSTALAR=(
-  snapd
-  mint-meta-codecs
-  winff
-  guvcview
+  arduino
+  gnome-tweaks
+  youtube-music
+  simulide
+  viper4linux-gui
+  gnome-extensions
+  dconf-editor
+  grub-customizer
+  gnome-clocks
+  gnome-weather
+  notable
+  plank
+  gimp
+  qbittorrent
+  vlc
+  telegram-desktop
+  ulauncher
+  appimagelauncher
+  pavucontrol
   virtualbox
-  flameshot
-  nemo-dropbox
   steam-installer
   steam-devices
   steam:i386
-  ratbagd
-  piper
   lutris
   libvulkan1
   libvulkan1:i386
@@ -52,13 +66,13 @@ sudo dpkg --add-architecture i386
 ## Atualizando o repositório ##
 sudo apt update -y
 
-## Adicionando repositórios de terceiros e suporte a Snap (Driver Logitech, Lutris e Drivers Nvidia)##
-sudo apt-add-repository "$PPA_LIBRATBAG" -y
+## Adicionando repositórios de terceiros (Lutris, Wine e Viper)##
 sudo add-apt-repository "$PPA_LUTRIS" -y
-sudo apt-add-repository "$PPA_GRAPHICS_DRIVERS" -y
+curl -s compressed "$URL_VIPER4LINUX_KEY | sudo apt-key add -"
+sudo curl -s --compressed -o /etc/apt/sources.list.d/thepbone_ppa.list "$URL_PPA_VIPER4LINUX"
 wget -nc "$URL_WINE_KEY"
 sudo apt-key add winehq.key
-sudo apt-add-repository "deb $URL_PPA_WINE bionic main"
+sudo apt-add-repository "deb $URL_PPA_WINE focal main"
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
@@ -67,10 +81,9 @@ sudo apt update -y
 
 ## Download e instalaçao de programas externos ##
 mkdir "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_GOOGLE_CHROME"       -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_SIMPLE_NOTE"         -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_4K_VIDEO_DOWNLOADER" -P "$DIRETORIO_DOWNLOADS"
-wget -c "$URL_INSYNC"              -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_GITHUB_DESKTOP"       -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_WEB_TORRENT_DESKTOP"  -P "$DIRETORIO_DOWNLOADS"
+wget -c "$URL_4K_VIDEO_DOWNLOADER"  -P "$DIRETORIO_DOWNLOADS"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
@@ -87,13 +100,13 @@ done
 sudo apt install --install-recommends winehq-stable wine-stable wine-stable-i386 wine-stable-amd64 -y
 
 ## Instalando pacotes Flatpak ##
-flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub com.rafaelmardojai.Blanket -y
 
 ## Instalando pacotes Snap ##
-sudo snap install spotify
-sudo snap install slack --classic
-sudo snap install skype --classic
-sudo snap install photogimp
+sudo snap install discord
+sudo snap install whatsapp-for-linux
+sudo snap install code
+
 # ---------------------------------------------------------------------- #
 
 # ----------------------------- PÓS-INSTALAÇÃO ----------------------------- #
