ff3766fcdb8b8c3aa9da089294ac60b1eccf6cc2
Stalkerfish <silva.allison2002@gmail.com>
2021-07-17 01:28:01 -0300

    fixed wrong link at wget and add autoclean repos

--- installViper.sh
+++ installViper.sh
@@ -2,7 +2,7 @@
 # ----------------------------- VARIÁVEIS ----------------------------- #
 
 
-URL_VIPER4LINUX="https://github.com/ThePBone/PPA-Repository/blob/master/viper4linux-gui_2.2-43.deb"
+URL_VIPER4LINUX="https://github.com/ThePBone/PPA-Repository/raw/master/viper4linux-gui_2.2-43.deb"
 
 URL_VIPER4LINUX_REPO="https://github.com/noahbliss/Viper4Linux.git"
 URL_GST_REPO="https://github.com/noahbliss/gst-plugin-viperfx"
@@ -24,14 +24,17 @@ PROGRAMAS_PARA_INSTALAR=(
 
 # ----------------------------- REQUISITOS ----------------------------- #
 
-cd ~
+sudo rm /var/lib/dpkg/lock-frontend
+sudo rm /var/cache/apt/archives/lock
 
-wget -c "$URL_VIPER4LINUX" 
+cd ~
 
 git clone "$URL_VIPER4LINUX_REPO"
 git clone "$URL_GST_REPO"
 git clone "$URL_VIPERFX_CORE_BINARY_REPO"
 
+wget -c "$URL_VIPER4LINUX" -P ~/
+sleep 5
 
 # ----------------------------- EXECUÇÃO ----------------------------- #
 
@@ -39,7 +42,7 @@ sudo apt update -y
 
 for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
   if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
-    apt install "$nome_do_programa" -y
+   sudo apt install "$nome_do_programa" -y
   else
     echo "[INSTALADO] - $nome_do_programa"
   fi
@@ -62,12 +65,15 @@ cp -r viper4linux ~/.config
 sudo cp viper /usr/local/bin
 
 cd ..
-sudo apt update -y
 
 cd ~
 
 sudo dpkg -i viper4linux-gui_2.2-43.deb
 
-sudo apt install qtbase5-dev libgstreamer-plugins-bad1.0-dev libgstreamer-opencv1.0-0 libgstreamer-plugins-good1.0-dev gir1.2-gst-plugins-bad-1.0 libopencv-dev libglu1-mesa-dev libqt5concurrent5 libqt5sql5 libqt5test5 libvulkan-dev libxext-dev qt5-qmake qtbase5-dev-tools qtchooser libqt5opengl5-dev
+sudo apt install qtbase5-dev libgstreamer-plugins-bad1.0-dev libgstreamer-opencv1.0-0 libgstreamer-plugins-good1.0-dev gir1.2-gst-plugins-bad-1.0 libopencv-dev libglu1-mesa-dev libqt5concurrent5 libqt5sql5 libqt5test5 libvulkan-dev libxext-dev qt5-qmake qtbase5-dev-tools qtchooser libqt5opengl5-dev -y
 
 sudo apt --fix-broken install
+
+sudo rm -r gst-plugin-viperfx Viper4Linux viperfx_core_binary
+
+echo "Viper4Linux was installed succesfully!
